43ce38618a3fd8bec719d4d5b43708aa1b01b661
allangpio <gpioallan@gmail.com>
2020-09-01 18:38:32 -0300

    connecting to the GitHub API and displaying searched repos

--- src/pages/Dashboard.tsx/index.tsx
+++ src/pages/Dashboard.tsx/index.tsx
@@ -1,70 +1,64 @@
-import React from 'react';
-
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
+
 import { Title, Form, Repositories } from './styles';
 import logoImg from '../../assets/logo.svg';
 
+interface Repository {
+  description: string;
+  full_name: string;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
 const Dashboard: React.FC = () => {
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+  const [newRepo, setNewRepo] = useState('');
+
+  async function handleAddRepostiroty(
+    event: FormEvent<HTMLFormElement>,
+  ): Promise<void> {
+    event.preventDefault();
+
+    const response = await api.get<Repository>(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logoImg} alt="logo" />
       <Title>Explore repositórios no GitHub</Title>
-      <Form>
+      <Form onSubmit={handleAddRepostiroty}>
         <input
-          type="text"
+          value={newRepo}
+          onChange={e => setNewRepo(e.target.value)}
           placeholder="Digite o nome do repositório"
-          name=""
-          id=""
         />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="#">
-          <img
-            src="https://avatars1.githubusercontent.com/u/63213995?s=460&u=06c696b4fb4c2795ba97e524c580308cb7d591be&v=4"
-            alt="Profile Pic"
-          />
-          <div>
-            <strong>allangpio/pontesdaterra-API</strong>
-            <p>
-              Backend of the Ponte da Terra app to list and store local farmers
-              information.
-            </p>
-          </div>
+        {repositories.map(repository => (
+          <a key={repository.full_name} href="#">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
 
-          <FiChevronRight size={20} />
-        </a>
-        <a href="#">
-          <img
-            src="https://avatars1.githubusercontent.com/u/63213995?s=460&u=06c696b4fb4c2795ba97e524c580308cb7d591be&v=4"
-            alt="Profile Pic"
-          />
-          <div>
-            <strong>allangpio/pontesdaterra-API</strong>
-            <p>
-              Backend of the Ponte da Terra app to list and store local farmers
-              information.
-            </p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
-        <a href="#">
-          <img
-            src="https://avatars1.githubusercontent.com/u/63213995?s=460&u=06c696b4fb4c2795ba97e524c580308cb7d591be&v=4"
-            alt="Profile Pic"
-          />
-          <div>
-            <strong>allangpio/pontesdaterra-API</strong>
-            <p>
-              Backend of the Ponte da Terra app to list and store local farmers
-              information.
-            </p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
